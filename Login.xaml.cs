19931d59f2dd460dee705c32059d315039ab15cd
CarlosFlorit <79317978+CarlosFlorit@users.noreply.github.com>
2022-02-13 00:59:42 +0100

    Add files via upload

---
+++ Login.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Recompensapp01
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public bool UserOK { get; set; }
+
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void check_Click(object sender, RoutedEventArgs e)
+        {
+            UserOK = false;
+            //Creamos una instancia de la base de datos que hemos creado (UserEntities)
+            DatabaseEntitiesAlumnos basededatos = new DatabaseEntitiesAlumnos();
+            Profesor usuario = basededatos.Profesor.SingleOrDefault(
+            us => us.usr.Equals(
+            usuar.Text, StringComparison.InvariantCultureIgnoreCase)
+            );
+
+            if (usuario != null)
+            {
+
+                string passHash = CalcHash(password.Password);
+                if (passHash.Equals(usuario.psw))
+                {
+                    UserOK = true; //Si coinciden UserOK=true y pasamos el control
+                                   //a la pantalla principal
+                }
+                else
+                { //Si no coinciden, mostramos el error y cerramos la aplicación.
+                    MessageBox.Show("Contraseña " + passHash + " no válidos");
+                    DialogResult = true;
+                    return;
+                }
+                DialogResult = true;
+            }
+        }
+
+        private string CalcHash(string contra)
+        {
+            UTF8Encoding enc = new UTF8Encoding();
+            byte[] data = enc.GetBytes(contra);
+            byte[] result;
+            SHA256CryptoServiceProvider sha = new SHA256CryptoServiceProvider();
+            result = sha.ComputeHash(data);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                // Convertimos los valores en hexadecimal
+                // cuando tiene una cifra hay que rellenarlo con cero
+                // para que siempre ocupen dos dígitos.
+                if (result[i] < 16)
+                {
+                    sb.Append("0");
+                }
+                sb.Append(result[i].ToString("x"));
+            }
+            return sb.ToString().ToUpper();
+           
+
+        }
+
+
+
+    }
+
+ 
+    }
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,583 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+
+namespace Recompensapp01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int puntosToAdd = 0;
+        int valorVentajas = 0;
+
+
+        public MainWindow()
+        {
+            
+            if (!CallLogin())
+            {
+                Application.Current.Shutdown(-1);
+            }
+            
+
+
+            //FireBaseHelper firebaseHelper = new FireBaseHelper(); No utilizado
+            InitializeComponent();
+            Mostrar_Datos();
+   
+
+
+
+
+        }
+
+
+
+        private void Mostrar_Datos()
+        {
+            DatabaseEntitiesAlumnos entidades = new DatabaseEntitiesAlumnos();
+
+
+
+
+            List<Alumno> alumnos = entidades.Alumno.ToList<Alumno>();
+
+            List<Ventaja> ventajasObtenidas = entidades.Ventaja.ToList<Ventaja>();
+
+
+            lstAlumnos.Items.Clear();
+            lstAlumnos.ItemsSource = alumnos;
+            lstAlumnos.DisplayMemberPath = "Apellidos_alumno";
+            lstAlumnos.SelectedValuePath = "Apellidos_alumno";
+            lstAlumnos.Items.Refresh();
+        }
+
+
+        private bool CallLogin()
+        {
+            Window1 login = new Window1();
+            login.ShowDialog();
+            //Si la pantalla de login proporciona un valor true a la variable UserOK
+            //gestionamos la identidad del usuario
+            if (login.UserOK)
+            {
+                //Para trabajar con usuarios que no son de Windows, sino de la BBDD
+                AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.UnauthenticatedPrincipal
+                );
+                //Crear una identidad a partir del usuario (la cadena “Database”
+                //simplemente nos indica de dónde hemos obtenido el usuario
+                IIdentity identity = new GenericIdentity(login.usuar.Text, "Database");
+                //Definimos unos roles, aunque por ahora nos los utilizaremos. La clase
+                //GeneriPrincipal nos los exige.
+                string[] roles = { "Usuario", "Administrador" };
+                GenericPrincipal credencial = new GenericPrincipal(identity, roles);
+                //Asignar esta credencial a la aplicación
+                //Introducimos en la variable del sistema
+                //System.Threading.Thread.CurrentPrincipal la identidad obtenida
+                //con sus roles.
+                System.Threading.Thread.CurrentPrincipal = credencial;
+            }
+            return login.UserOK;
+        }
+
+
+        //Seleccionar alumno en el combobox y mostrar datos
+        private void lstAlumnos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DatabaseEntitiesAlumnos entidades = new DatabaseEntitiesAlumnos();
+            //int selectedIndex = lstAlumnos.SelectedIndex;
+            //Object selectedItem = lstAlumnos.SelectedItem;
+
+            //Alumno alumno = entidades.Alumno.ToList<Alumno>().Where(c => c.Equals(selectedItem)).FirstOrDefault<Alumno>();
+
+            Alumno alumno = lstAlumnos.SelectedItem as Alumno;
+
+            //Obtencion obtencion = object as Obtencion;
+
+            boxNia.Text = alumno.Nia;
+            boxCurso.Text = alumno.Curso;
+            boxGrupo.Text = alumno.Grupo;
+            boxPuntos.Text = Convert.ToString(alumno.Saldo);
+
+            puntosNombreAlumno.Text = alumno.Apellidos_alumno;
+            puntosNiaAlumno.Text = alumno.Nombre_alumno;
+            boxSaldoActual.Text = Convert.ToString(alumno.Saldo);
+
+            ventajasSaldoActual.Text = Convert.ToString(alumno.Saldo);
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+
+
+
+            Obtencion ventajasObt = entidades.Obtencion.FirstOrDefault(c => c.niaAlumno.Equals(alumno.Nia));
+
+
+
+
+            List<Obtencion> ventajasObtenidas = (from obt in entidades.Obtencion
+                                                 where obt.niaAlumno == alumno.Nia
+                                                 select obt).ToList();
+
+
+            try
+            {
+
+
+                List<Ventaja> ventajas = (from obt in entidades.Ventaja
+                                          where obt.idVentaja == ventajasObt.idrVentaja
+                                          select obt).ToList();
+
+
+                lstVentajas.ItemsSource = ventajas;
+                lstVentajas.DisplayMemberPath = "nombreVentaja";
+                lstVentajas.SelectedValuePath = "idVentaja";
+            }
+
+            catch
+            {
+                MessageBox.Show("El alumno todavía no ha adquirido ninguna ventaja");
+                lstVentajas.ItemsSource = null;
+                //lstAlumnos.Items.Clear();
+            }
+
+
+            /*
+            if (alumno != null) { 
+            Obtencion ventajasObtenidas = entidades.Obtencion.ToList<Obtencion>().Where(c => int.Parse(c.niaAlumno) == int.Parse(alumno.Nia)).FirstOrDefault<Obtencion>();
+
+            lstVentajas.Items.Clear();
+            lstVentajas.ItemsSource = ventajasObtenidas;
+            lstAlumnos.Items.Refresh();
+
+            }
+            else
+            {
+                MessageBox.Show("No hay ninguna coincidencia");
+            }
+
+            */
+
+        }
+
+
+        //
+        //Comprobación de checkboxes para saldo 
+        private void Check1_Checked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd + 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd - 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check2_Checked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd + 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd - 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check3_Checked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd + 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check3_Unchecked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd - 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check4_Checked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd + 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check4_Unchecked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd - 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check5_Checked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd + 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check5_Unchecked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd - 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check6_Checked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd + 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+
+        private void Check6_Unchecked(object sender, RoutedEventArgs e)
+        {
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+            puntosToAdd = puntosToAdd - 10;
+            boxSaldoAdd.Text = Convert.ToString(puntosToAdd);
+        }
+        //Fin de comprobaciones para checkboxes de añadir saldo
+
+
+
+        //
+        //Botón añadir saldo
+        private void AddSaldo(object sender, RoutedEventArgs e)
+        {
+            DatabaseEntitiesAlumnos entidades = new DatabaseEntitiesAlumnos();
+            Alumno alumno = lstAlumnos.SelectedItem as Alumno;
+            alumno.Saldo = alumno.Saldo + puntosToAdd;
+
+            entidades.SaveChanges();
+            boxSaldoActual.Text = Convert.ToString(alumno.Saldo);
+            ventajasSaldoActual.Text = Convert.ToString(alumno.Saldo);
+
+            //Ponemos checkboxes de saldo en blanco
+            Check1.IsChecked = false;
+            Check2.IsChecked = false;
+            Check3.IsChecked = false;
+            Check4.IsChecked = false;
+            Check5.IsChecked = false;
+            Check6.IsChecked = false;
+        }
+
+
+
+        //
+        //Comprobación de checkboxes para "precio" de obtención de ventajas
+        //
+        private void vCheck1_Checked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas + 80;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+        private void vCheck1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas - 80;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+        private void vCheck2_Checked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas + 20;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+
+        private void vCheck2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas - 20;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+        private void vCheck3_Checked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas + 40;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+
+        private void vCheck3_Unchecked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas - 40;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+        private void vCheck4_Checked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas + 80;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+
+        private void vCheck4_Unchecked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas - 80;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+        private void vCheck5_Checked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas + 40;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+
+        private void vCheck5_Unchecked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas - 40;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+        private void vCheck6_Checked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas + 10;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+
+
+        private void vCheck6_Unchecked(object sender, RoutedEventArgs e)
+        {
+            valorVentajas = valorVentajas - 10;
+            ventajasPrecio.Text = Convert.ToString(valorVentajas);
+        }
+        //Fin de comprobación de checkboxes de ventajas
+        //
+
+
+        //Añadir las ventajas seleccionadas mediante botón
+        private void AddVent(object sender, RoutedEventArgs e)
+        {
+
+            //Comprobamos si hay saldo suficiente
+            if (valorVentajas > int.Parse(boxSaldoActual.Text)) 
+            
+            {
+                MessageBox.Show("No tiene saldo suficiente");
+
+            }
+
+            //Si hay saldo suficiente:
+            else { 
+
+                //Actualizamos saldo actual del alumno
+                DatabaseEntitiesAlumnos entidades = new DatabaseEntitiesAlumnos();
+
+                Alumno alu = entidades.Alumno.ToList<Alumno>().Where(c => c.Nia.Equals(boxNia.Text)).FirstOrDefault<Alumno>();
+                alu.Saldo = alu.Saldo - valorVentajas;
+                entidades.SaveChanges();
+                boxSaldoActual.Text = Convert.ToString(alu.Saldo);
+                ventajasSaldoActual.Text = Convert.ToString(alu.Saldo);
+
+
+                //Añadimos las ventajas seleccionadas en los checkboxes
+                if ((bool)vCheck1.IsChecked == true) 
+                {
+                
+                    Obtencion vent1 = new Obtencion();
+                    vent1.niaAlumno = boxNia.Text;
+                    vent1.idrVentaja = 1;
+
+                    try
+                    {
+                        entidades.Obtencion.AddObject(vent1);
+                        entidades.SaveChanges();
+                        MessageBox.Show("Ventaja añadida con éxito");
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error al añadir ventaja");
+                    }
+
+                }
+
+                if ((bool)vCheck2.IsChecked == true)
+                {
+                
+                    Obtencion vent2 = new Obtencion();
+                    vent2.niaAlumno = boxNia.Text;
+                    vent2.idrVentaja = 2;
+
+                    try{ 
+                    entidades.Obtencion.AddObject(vent2);
+                    entidades.SaveChanges();
+                    MessageBox.Show("Ventaja añadida con éxito");
+
+                    }
+
+                    catch
+                    {
+                        MessageBox.Show("Error al añadir ventaja");
+                    }
+                }
+
+                if ((bool)vCheck3.IsChecked == true)
+                {
+                
+                    Obtencion vent3 = new Obtencion();
+                    vent3.niaAlumno = boxNia.Text;
+                    vent3.idrVentaja = 3;
+
+                    try
+                    {
+                        entidades.Obtencion.AddObject(vent3);
+                        entidades.SaveChanges();
+                        MessageBox.Show("Ventaja/s añadida/s con éxito");
+
+                    }
+
+                    catch
+                    {
+                        MessageBox.Show("Error al añadir ventaja");
+                    }
+                }
+
+                if ((bool)vCheck4.IsChecked == true)
+                {
+                
+                    Obtencion vent4 = new Obtencion();
+                    vent4.niaAlumno = boxNia.Text;
+                    vent4.idrVentaja = 4;
+
+                    try
+                    {
+                        entidades.Obtencion.AddObject(vent4);
+                        entidades.SaveChanges();
+                        MessageBox.Show("Ventaja añadida con éxito");
+
+                    }
+
+                    catch
+                    {
+                        MessageBox.Show("Error al añadir ventaja");
+                    }
+                }
+
+
+                if ((bool)vCheck5.IsChecked == true)
+                {
+               
+                    Obtencion vent5 = new Obtencion();
+                    vent5.niaAlumno = boxNia.Text;
+                    vent5.idrVentaja = 5;
+
+                    try
+                    {
+                        entidades.Obtencion.AddObject(vent5);
+                        entidades.SaveChanges();
+                        MessageBox.Show("Ventaja añadida con éxito");
+
+                    }
+
+                    catch
+                    {
+                        MessageBox.Show("Error al añadir ventaja");
+                    }
+                }
+
+
+                if ((bool)vCheck6.IsChecked == true)
+                {
+                
+                    Obtencion vent6 = new Obtencion();
+                    vent6.niaAlumno = boxNia.Text;
+                    vent6.idrVentaja = 6;
+
+                    try
+                    {
+                        entidades.Obtencion.AddObject(vent6);
+                        entidades.SaveChanges();
+                        MessageBox.Show("Ventaja añadida con éxito");
+
+                    }
+
+                    catch
+                    {
+                        MessageBox.Show("Error al añadir ventaja");
+                    }
+                }
+
+
+                //Limpiamos los checkboxes
+                vCheck1.IsChecked = false;
+                vCheck2.IsChecked = false;
+                vCheck3.IsChecked = false;
+                vCheck4.IsChecked = false;
+                vCheck5.IsChecked = false;
+                vCheck6.IsChecked = false;
+
+
+                //Actualizamos la lista de ventajas
+
+                Alumno alumno = lstAlumnos.SelectedItem as Alumno;
+
+                Obtencion ventajasObt = entidades.Obtencion.FirstOrDefault(c => c.niaAlumno.Equals(alumno.Nia));
+
+
+
+
+                List<Obtencion> ventajasObtenidas = (from obt in entidades.Obtencion
+                                                 where obt.niaAlumno == alumno.Nia
+                                                 select obt).ToList();
+
+                try
+                {
+
+                    
+                    List<Ventaja> ventajas = (from obt in entidades.Ventaja
+                                          where obt.idVentaja == ventajasObt.idrVentaja
+                                          select obt).ToList();
+
+
+                    lstVentajas.DisplayMemberPath = "nombreVentaja";
+                    lstVentajas.SelectedValuePath = "idVentaja";
+                    lstVentajas.ItemsSource = ventajas;
+                    lstVentajas.Items.Refresh();
+                }
+
+                catch
+                {
+                    MessageBox.Show("Error al actualizar la lista de ventajas");
+                    //lstVentajas.ItemsSource = null;
+                    //lstAlumnos.Items.Clear();
+                }
+
+
+            }
+
+        }
+    }
+}
